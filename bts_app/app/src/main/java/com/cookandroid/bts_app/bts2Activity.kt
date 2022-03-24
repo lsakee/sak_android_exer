938b7af7e46119451dfb00a8f170e7f1371abd74
esak <lsls4868@gmail.com>
2022-03-25 03:31:23 +0900

    android with kotlin

---
+++ bts_app/app/src/main/java/com/cookandroid/bts_app/bts2Activity.kt
@@ -0,0 +1,15 @@
+package com.cookandroid.bts_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+
+class bts2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bts2)
+
+    }
+}
