cfa86427cc07b62e4fc64b65117bbd1f998abfaa
esak <lsls4868@gmail.com>
2022-05-02 20:16:55 +0900

    android ex

---
+++ Project6_2/app/src/main/java/com/cookandroid/project6_2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.cookandroid.project6_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtUrl;
+    Button btnGo,btnBack;
+    WebView web;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("이삭");
+        edtUrl = findViewById(R.id.edtUrl);
+        btnGo=findViewById(R.id.btnGo);
+        btnBack=findViewById(R.id.btnBack);
+        web=findViewById(R.id.webView1);
+        web.setWebViewClient(new CookWebViewClient());
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.loadUrl(edtUrl.getText().toString());
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.goBack();
+            }
+        });
+    }
+
+    class CookWebViewClient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+}
